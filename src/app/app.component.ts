d16adcae5ca909f22a94aa6e62baa7c1f498125b
Lindsey Sandberg <lindsey.sandberg@concordusa.com>
2019-10-22 13:17:21 -0500

    adds http routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   template: `
-  <div> 
-    <h1> {{pageTitle}}</h1>
-    <pm-products></pm-products>
-  </div>
-  `
-})
 
-export class AppComponent{
-  pageTitle: string = 'Acme Production Management';
+    <div class='container'>
+      <router-outlet></router-outlet>
+    </div>
+    `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  pageTitle = 'Acme Product Management';
 }
+
+
+//     <nav class='navbar navbar-expand navbar-light bg-light'>
+// <a class='navbar-brand'>{{pageTitle}}</a>
+// <ul class='nav nav-pills'>
+//   <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/welcome']">Home</a></li>
+//   <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/products']">Product List</a></li>
+// </ul>
+// </nav>
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,71 +1,56 @@
-import { Component, OnInit } from "@angular/core";
-import { IProduct } from './product'
+import { Component, OnInit } from '@angular/core';
+
+import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
-    selector: 'pm-products',
-    templateUrl: './product-list.component.html',
-    styleUrls: ['./product-list.component.css']
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
-
 export class ProductListComponent implements OnInit {
-    pageTitle: string = 'Product List';
-    imageWidth: number = 50;
-    imageMargin: number = 2;
-    showImage: boolean = false;
-    _listFilter: string;
-    get listFilter(): string {
-        return this._listFilter;
-    }
-    set listFilter(value: string) {
-        this._listFilter = value;
-        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
-    }
+  pageTitle = 'Product List';
+  imageWidth = 50;
+  imageMargin = 2;
+  showImage = false;
+  errorMessage = '';
 
-    filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2019",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2019",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        },
+  _listFilter = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
 
-    ];
+  filteredProducts: IProduct[] = [];
+  products: IProduct[] = [];
 
-    performFilter(filterBy: string): IProduct[] {
-        filterBy.toLocaleLowerCase();
-        return this.products.filter((product: IProduct) =>
-            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1)
-    }
+  constructor(private productService: ProductService) {
 
-    onRatingClicked(message: string): void {
-        this.pageTitle = 'Product List: ' + message;
-        console.log(this.pageTitle);
+  }
 
-    }
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
 
-    toggleImage(): void {
-        this.showImage = !this.showImage;
-    }
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
 
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 
-
-    ngOnInit(): void {
-        console.log('In OnInit');
-
-    }
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
+  }
 }
