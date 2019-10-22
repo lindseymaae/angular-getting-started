3ff97e4e83fb10891664ad3f855a89adfb647b61
Lindsey Sandberg <lindsey.sandberg@concordusa.com>
2019-10-22 15:01:15 -0500

    finishes tutorial

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-// import { ProductDetailGuard } from './product-detail.guard';
+import { ProductDetailGuard } from './product-detail.guard';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
@@ -13,7 +13,7 @@ import { SharedModule } from '../shared/shared.module';
       { path: 'products', component: ProductListComponent },
       {
         path: 'products/:id',
-        // canActivate: [ProductDetailGuard],
+        canActivate: [ProductDetailGuard],
         component: ProductDetailComponent
       }
     ]),
